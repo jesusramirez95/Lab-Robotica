e662f3b83f12de70cdb3b2b04fc7868300b4079e
jesusramirez95 <jesusa.ramirez@udem.edu>
2018-04-17 01:55:31 -0500

    Se añade la practica 9 y la practica 2 acomodada

--- practica9/hough_lines.py
+++ practica9/hough_lines.py
@@ -15,107 +15,116 @@ import math
 
 def draw_lines(img,lines):
     # In case of error, don't draw the line
-    draw_right = True
-    draw_left = True
+	draw_right = True
+	draw_left = True
     
     # Find slopes of all lines
     # But only care about lines where abs(slope) > slope_threshold
-    slope_threshold = 0.5
-    slopes = []
-    new_lines = []
-    for line in lines:
-        x1, y1, x2, y2 = line[0]  # line = [[x1, y1, x2, y2]]
+	slope_threshold = 0.5
+	slopes = []
+	new_lines = []
+	for line in lines:
+		x1, y1, x2, y2 = line[0]  # line = [[x1, y1, x2, y2]]
         
         # Calculate slope
-        if x2 - x1 == 0.:  # corner case, avoiding division by 0
-            slope = 999.  # practically infinite slope
-        else:
-            slope = (y2 - y1) / (x2 - x1)
+		if x2 - x1 == 0.:  # corner case, avoiding division by 0
+			slope = 999.  # practically infinite slope
+		else:
+			slope = (y2 - y1) / (x2 - x1)
             
         # Filter lines based on slope
-        if abs(slope) > slope_threshold:
-            slopes.append(slope)
-            new_lines.append(line)
+		if abs(slope) > slope_threshold:
+			slopes.append(slope)
+			new_lines.append(line)
         
-    lines = new_lines
+	lines = new_lines
     
     # Split lines into right_lines and left_lines, representing the right and left lane lines
     # Right/left lane lines must have positive/negative slope, and be on the right/left half of the image
-    right_lines = []
-    left_lines = []
-    for i, line in enumerate(lines):
-        x1, y1, x2, y2 = line[0]
-        img_x_center = img.shape[1] / 2  # x coordinate of center of image
-        if slopes[i] > 0 and x1 > img_x_center and x2 > img_x_center:
-            right_lines.append(line)
-        elif slopes[i] < 0 and x1 < img_x_center and x2 < img_x_center:
-            left_lines.append(line)
+	right_lines = []
+	left_lines = []
+	for i, line in enumerate(lines):
+		x1, y1, x2, y2 = line[0]
+		img_x_center = img.shape[1] / 2  # x coordinate of center of image
+		if slopes[i] > 0 and x1 > img_x_center and x2 > img_x_center:
+			right_lines.append(line)
+		elif slopes[i] < 0 and x1 < img_x_center and x2 < img_x_center:
+			left_lines.append(line)
             
     # Run linear regression to find best fit line for right and left lane lines
     # Right lane lines
-    right_lines_x = []
-    right_lines_y = []
+	right_lines_x = []
+	right_lines_y = []
     
-    for line in right_lines:
-        x1, y1, x2, y2 = line[0]
+	for line in right_lines:
+		x1, y1, x2, y2 = line[0]
         
-        right_lines_x.append(x1)
-        right_lines_x.append(x2)
+		right_lines_x.append(x1)
+		right_lines_x.append(x2)
         
-        right_lines_y.append(y1)
-        right_lines_y.append(y2)
+		right_lines_y.append(y1)
+		right_lines_y.append(y2)
         
-    if len(right_lines_x) > 0:
-        right_m, right_b = np.polyfit(right_lines_x, right_lines_y, 1)  # y = m*x + b
-    else:
-        right_m, right_b = 1, 1
-        draw_right = False
+	if len(right_lines_x) > 0:
+		right_m, right_b = np.polyfit(right_lines_x, right_lines_y, 1)  # y = m*x + b
+	else:
+		right_m, right_b = 1, 1
+		draw_right = False
         
     # Left lane lines
-    left_lines_x = []
-    left_lines_y = []
+	left_lines_x = []
+	left_lines_y = []
     
-    for line in left_lines:
-        x1, y1, x2, y2 = line[0]
+	for line in left_lines:
+		x1, y1, x2, y2 = line[0]
         
-        left_lines_x.append(x1)
-        left_lines_x.append(x2)
+		left_lines_x.append(x1)
+		left_lines_x.append(x2)
         
-        left_lines_y.append(y1)
-        left_lines_y.append(y2)
+		left_lines_y.append(y1)
+		left_lines_y.append(y2)
         
-    if len(left_lines_x) > 0:
-        left_m, left_b = np.polyfit(left_lines_x, left_lines_y, 1)  # y = m*x + b
-    else:
-        left_m, left_b = 1, 1
-        draw_left = False
+	if len(left_lines_x) > 0:
+		left_m, left_b = np.polyfit(left_lines_x, left_lines_y, 1)  # y = m*x + b
+	else:
+		left_m, left_b = 1, 1
+		draw_left = False
     
     # Find 2 end points for right and left lines, used for drawing the line
     # y = m*x + b --> x = (y - b)/m
-    y1 = 850
-    y2 = 610
+	y1 = 850
+	y2 = 610
     
-    right_x1 = (y1 - right_b) / right_m
-    right_x2 = (y2 - right_b) / right_m
+	right_x1 = (y1 - right_b) / right_m
+	right_x2 = (y2 - right_b) / right_m
     
-    left_x1 = (y1 - left_b) / left_m
-    left_x2 = (y2 - left_b) / left_m
+	left_x1 = (y1 - left_b) / left_m
+	left_x2 = (y2 - left_b) / left_m
     
     # Convert calculated end points from float to int
-    y1 = int(y1)
-    y2 = int(y2)
-    right_x1 = int(right_x1)
-    right_x2 = int(right_x2)
-    left_x1 = int(left_x1)
-    left_x2 = int(left_x2)
+	y1 = int(y1)
+	y2 = int(y2)
+	right_x1 = int(right_x1)
+	right_x2 = int(right_x2)
+	left_x1 = int(left_x1)
+	left_x2 = int(left_x2)
     
     # Draw the right and left lines on image
-    if draw_right:
-        cv2.line(img, (right_x1, y1), (right_x2, y2), (0,0,255), 5)
-    if draw_left:
-        cv2.line(img, (left_x1, y1), (left_x2, y2), (0,0,255), 5)
+	if draw_right:
+		cv2.line(img, (right_x1, y1), (right_x2, y2), (0,0,255), 5)
+	if draw_left:
+		cv2.line(img, (left_x1, y1), (left_x2, y2), (0,0,255), 5)
+	if draw_right and draw_left:
+		overlay=img.copy()
+		pts=np.array([[right_x1, y1],[right_x2, y2],[left_x2, y2],[left_x1, y1]],np.int32)
+		pts=pts.reshape((-1,1,2))
+		cv2.fillPoly(overlay,[pts],(0,255,0))
+		opacity=.2
+		cv2.addWeighted(overlay, opacity, img, 1 - opacity, 0, img)
+		
+		
 	
-    return(img)
+	return(img)
 
 def region_of_interest(img, vertices):
     """
